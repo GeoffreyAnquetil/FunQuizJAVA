48413b9e4a303dd3cbf7992482b3f8c8435bdccb
GeoffreyAnquetil <geoffrey.anquetil1@gmail.com>
2023-05-05 16:25:05 +0200

    Ajout de la méthode toTab() à FileScan

--- src/FileScan.java
+++ src/FileScan.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class FileScan {
     private String path; // L'adresse du fichier texte
@@ -9,11 +10,20 @@ public class FileScan {
     private BufferedReader bufferedReader;
 
     /**
-     * Classe permettant de traiter des données sortantes d'un fichier texte (lecture...)
+     * Classe permettant de traiter des données entrantes et sortantes d'un fichier texte (lecture, écriture...)
      * @param path
      */
     public FileScan(String path){
         this.path = path;
+    }
+
+    /**
+     *
+     * @return Un tableau à deux dimensions avec en ligne les lignes du fichier text séparées en
+     *  colonnes par les ; dans le fichier texte
+     *
+     */
+    public ArrayList<String[]> toTab(){
         try {
             // Cette ligne peut causer une erreur de type IO si le fichier n'est pas trouvé
             this.fileReader = new FileReader(path);
@@ -21,15 +31,33 @@ public class FileScan {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        ArrayList<String[]> res = new ArrayList<String[]>();
+        try {
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                String[] ligne = line.split(";");
+                res.add(ligne);
+                line = bufferedReader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } return res;
     }
 
     /**
      * Print ligne par ligne le contenu du fichier ciblé
      */
-    public void printLineByLine(){
-        try{
+    public void printLineByLine() {
+        try {
+            // Cette ligne peut causer une erreur de type IO si le fichier n'est pas trouvé
+            this.fileReader = new FileReader(path);
+            bufferedReader = new BufferedReader(fileReader);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
             String line = bufferedReader.readLine();
-            while (line != null){
+            while (line != null) {
                 System.out.println(line);
                 line = bufferedReader.readLine();
             }
@@ -37,4 +65,5 @@ public class FileScan {
             e.printStackTrace();
         }
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         FileScan fileScan = new FileScan("./src/testfile.txt");
         fileScan.printLineByLine();
+        ArrayList<String[]> test = fileScan.toTab();
+
+        System.out.println();
+
+        for(String[] ligne : test){
+            for(String mot : ligne){
+                System.out.print(mot + " / ");
+            }
+            System.out.println();
+        }
     }
 }
