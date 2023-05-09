68d0831a17cb49b063440467a3dad1277a831aa2
GeoffreyAnquetil <geoffrey.anquetil1@gmail.com>
2023-05-09 14:09:05 +0200

    Suppression des classes FileScan et FileWrite qui sont en réalité inutiles pour le Projet. Ajout des classes Personne et Utilisateurs ainsi que les méthodes permettant de sérialiser et désérialiser les objets

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,25 @@
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
-    public static void main(String[] args) {
-        FileScan fileScan = new FileScan("./src/testfile.txt");
-        fileScan.printLineByLine();
-        ArrayList<String[]> test = fileScan.toTab();
+    public static void main(String[] args){
+        String path = "./src/testfile.txt";
+        Personne personne1 = new Personne("0001", "Hailwell", "mdp1", true, "PP1.png");
+        Personne personne2 = new Personne("0002", "Araonof", "mdp2", true, "PP2.png");
+        Personne personne3 = new Personne("0003", "Flowt", "mdp3", true, "PP3.png");
 
-        System.out.println();
+        HashMap<String, Personne> personnes = new HashMap<String, Personne>();
+        personnes.put(personne1.getId(), personne1);
+        personnes.put(personne2.getId(), personne2);
+        personnes.put(personne3.getId(), personne3);
 
-        System.out.println(fileScan.getData("IsAdmin","0001"));
-        System.out.println(fileScan.getData("Pseudo","0002"));
-        System.out.println(fileScan.getData("Caca","0001"));
-        System.out.println(fileScan.getData("IsAdmin","0003"));
+        Utilisateurs users1 = new Utilisateurs(personnes);
+        users1.serialize(path);
+
+        Utilisateurs users2 = new Utilisateurs();
+        users2.deserialize(path);
+
+        System.out.println(users2.getUsers().get("0002").getPseudo());
     }
 }
+
+
---
+++ src/Utilisateurs.java
@@ -0,0 +1,56 @@
+import java.io.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Classe permettant de stocker les utilisateurs inscrits au quiz dans un fichier texte
+ */
+public class Utilisateurs implements Serializable {
+    private HashMap<String, Personne> users;
+
+    public Utilisateurs(){}
+
+    public Utilisateurs(HashMap<String, Personne> users) {
+        this.users = users;
+    }
+
+    /**
+     * Sérialise l'objet dans un fichier donné
+     * @param pathToFile chemin d'accès au fichier dans lequel sérialiser l'objet
+     */
+    public void serialize(String pathToFile){
+        try{
+            FileOutputStream fichier = new FileOutputStream(pathToFile);
+            ObjectOutputStream oos = new ObjectOutputStream(fichier);
+            oos.writeObject(this.users);
+            oos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Désérialise un objet contenu dans un fichier
+     * @param pathToFile chemin d'accès au fichier où se trouve l'objet sérialisé
+     */
+    public void deserialize(String pathToFile){
+        HashMap<String, Personne> utilisateursDes = null;
+        try{
+            FileInputStream fichier = new FileInputStream(pathToFile);
+            ObjectInputStream ois = new ObjectInputStream(fichier);
+            utilisateursDes = (HashMap<String, Personne>)ois.readObject();
+            ois.close();
+        } catch (IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        } this.users = utilisateursDes;
+    }
+
+    public Map<String, Personne> getUsers() {
+        return users;
+    }
+
+    public void setUsers(HashMap<String, Personne> users) {
+        this.users = users;
+    }
+}
